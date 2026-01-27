ebfc5074213d7952e5c34315d460d762a4a8f133
JesuisJ <sw02096@icloud.com>
2026-01-27 21:18:20 +0100

    feat: add handler-based parsing for server directives

--- includes/ServerConfig.hpp
+++ includes/ServerConfig.hpp
@@ -6,7 +6,7 @@
 /*   By: princessj <princessj@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/27 17:31:12 by jihyeki2          #+#    #+#             */
-/*   Updated: 2026/01/27 18:26:28 by princessj        ###   ########.fr       */
+/*   Updated: 2026/01/27 20:41:19 by princessj        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 #include "Token.hpp" // <string>은 Token.hpp에 있음
 #include <vector>
+#include <stdexcept>
 
 class LocationConfig; // forward declaration: 순환 include 위험
 
@@ -28,7 +29,12 @@ class	ServerConfig
 		void	addLocation(const LocationConfig &location); // location은 server 내부에 종속: ServerConfig가 관리 및 내부에서 통제 가능(캡슐화)
 	
 	private:
-		/* 대표적인 server level 설정 : 추후 더 추가 예정 */
+		/* semantic handlersfuncs */
+		void	handleListen(const std::vector<Token> &tokens, size_t &i);
+		void	handleRoot(const std::vector<Token> &tokens, size_t &i);
+		void	handleErrorPage(const std::vector<Token> &tokens, size_t &i);
+	
+		/* 대표적인 server level 필드 - 추후 더 추가 예정 (TODO) 회의 해야함) */
 		std::vector<int>			_listenPorts; // ex) {80, 8080, 443}: 여러 소켓에 바인딩(포트 1개당 = 소켓 1개 / 서버 프로그램 1개 - 여러 포트 가능)
 		std::string					_root;
 		std::string					_errorPage; // error page path
